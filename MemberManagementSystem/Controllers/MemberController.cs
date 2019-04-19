e6b907d88e8d3793404641115eee31fe8e2f21dd
hanna.chung35 <hanna.chung35@TI03NB59>
2019-04-20 00:11:52 +0800

    add a index from MemberManagementSystem

---
+++ MemberManagementSystem/Controllers/MemberController.cs
@@ -0,0 +1,22 @@
+﻿using MemberManagementSystem.Models;
+using MemberManagementSystem.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberManagementSystem.Controllers
+{
+    public class MemberController : Controller
+    {
+        MemberService memberService = new MemberService();
+        // GET: Member
+        public ActionResult Index()
+        {
+            List<MemberData> MemberLsit = new List<MemberData>();
+            MemberLsit = memberService.GetAllMemberData();
+            return View(MemberLsit);
+        }
+    }
+}
---
+++ MemberManagementSystem/Service/MemberService.cs
@@ -0,0 +1,18 @@
+﻿using MemberManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberManagementSystem.Service
+{
+    public class MemberService
+    {
+        MemberManagementSystemEntities db = new MemberManagementSystemEntities();
+
+        public List<MemberData> GetAllMemberData()
+        {
+            return db.MemberData.ToList();
+        }
+    }
+}
